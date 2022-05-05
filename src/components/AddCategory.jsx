68a65b0efdb0f058d2149eb64caa48be6fe486fe
Lucia <luciafutestti@gmail.com>
2022-05-05 12:54:07 -0300

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const AddCategory = ({setCategories}) => {
+
+  const [inputValue, setInputValue] = useState('hola');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCategories(categories => [...categories, inputValue]);
+
+    }
+
+
+  return (
+    <>
+        <form onSubmit={handleSubmit}>
+            <input
+            type="text" 
+            value={inputValue}
+            onChange={handleInputChange}
+            />  
+        </form>
+    </>
+  )
+}
+
+export default AddCategory;
